8b64255100f9a956a6429c2e4602ee3025682cf7
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2023-09-14 21:41:21 +0200

    Update ex3.php

--- P2/ex3.php
+++ P2/ex3.php
@@ -8,3 +8,22 @@ $recipe = [
 foreach ($recipe as $property => $propertyValue) {
     echo '[' . $property . '] vaut ' . $propertyValue . PHP_EOL;
 }
+
+// php scrip to get datas from random api 
+
+// Path: P2/ex4.php
+
+<?php
+$curl = curl_init('https://jsonplaceholder.typicode.com/users');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$data = curl_exec($curl);
+if ($data === false) {
+    var_dump(curl_error($curl));
+} else {
+    $data = json_decode($data, true);
+    foreach ($data as $user) {
+        echo $user['name'] . PHP_EOL;
+    }
+}
+
+// php script to get datas from random api and save it in a file
